32c2524807f28acd2cf2a2d3ba28c5b7e014e6bd
raybloo <boris.zbinden@epfl.ch>
2017-03-02 18:18:50 +0100

    External and common added

---
+++ hw1/build/CMakeFiles/Homework1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Homework1.dir/main.cpp.obj"
+  "Homework1.pdb"
+  "Homework1.exe"
+  "Homework1.exe.manifest"
+  "Homework1.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Homework1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
