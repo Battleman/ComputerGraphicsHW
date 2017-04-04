637c7cc0fcdc198b361e99f87909950a54cb1e45
Olivier Jeremie Cloux <cloux@icin3pc37.epfl.ch>
2017-04-04 10:42:42 +0200

    few first functions...

--- Projet/project/quad/quad_vshader.glsl
+++ Projet/project/quad/quad_vshader.glsl
@@ -6,8 +6,18 @@ in vec2 vtexcoord;
 out vec2 uv;
 
 uniform mat4 MVP;
+uniform float permutation[256];
+
+float mix(float x, float y , float f) {
+  return (1 - f)*x + f*y;
+}
+
+float fun(float t) {
+  return t*t*t*(t*(t*6.0 -15.0) + 10.0);
+}
 
 void main() {
+
     gl_Position = MVP * vec4(position[0],position[1],0.0,1.0);
     uv = (position + vec2(1.0, 1.0)) * 0.5;
 }
