83e4da2029d42cd162bc15ac0681d00e782af3c6
Pinedo Alvaro <alvaropinedo22@yahoo.es>
2017-04-11 10:48:44 +0200

    Water level applied

--- Projet/project/quad/quad_vshader.glsl
+++ Projet/project/quad/quad_vshader.glsl
@@ -18,7 +18,11 @@ void main() {
     vec2 uv = (position + vec2(1.0, 1.0)) * 0.5;
     float height = texture(tex,uv).r;
     //gl_Position = MVP * vec4(position[0], position[1], texture(tex, uv).r, 1.0);
-    vpoint_mv = MV * vec4(position[0],position[1],height, 1.0);
+    if(height < -0.45) {
+        vpoint_mv = MV * vec4(position[0],position[1],-0.45, 1.0);
+    } else {
+        vpoint_mv = MV * vec4(position[0],position[1],height, 1.0);
+    }
     gl_Position = projection * vpoint_mv;
     light_dir = normalize(light_pos-vec3(vpoint_mv));
     view_dir = normalize(-vec3(vpoint_mv));
