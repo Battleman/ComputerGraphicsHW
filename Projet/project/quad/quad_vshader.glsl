e2b796bdd7dc5a7cefa26977ae292af9dac35593
raybloo <boris.zbinden@epfl.ch>
2017-04-13 21:14:58 +0200

    Added textures for rock, forest, sand

--- Projet/project/quad/quad_vshader.glsl
+++ Projet/project/quad/quad_vshader.glsl
@@ -12,6 +12,9 @@ uniform mat4 projection;
 uniform mat4 model;
 uniform mat4 view;
 uniform sampler2D tex;
+uniform sampler2D tex2;
+uniform sampler2D tex3;
+uniform sampler2D tex4;
 uniform vec3 light_pos;
 uniform int triangles_number;
 
@@ -37,5 +40,39 @@ void main() {
     vec3 Y = Y1point_mv.xyz - Y0point_mv.xyz;
     normal_mv = normalize(cross(X,Y));
     */
+
+    //Next I'll be using a gaussian function to make the color changes smoother
+    vec3 height_material = vec3(0.0,0.0,0.0);
+    float variance = 0.0;
+    float color_height = 0.0;
+
+    //sand: yellow, at height -0.40, spread: -0.45 to -0.40
+    variance = 0.05;
+    color_height = -0.45;
+    height_material += exp((-pow((height-color_height),2))/(2*variance*variance)) * texture(tex4,uv).rgb;
+    //dirt: brown, at height -0.20, spread: -0.40 to -0.30
+    variance = 0.05;
+    color_height = -0.35;
+    height_material += exp((-pow((height-color_height),2))/(2*variance*variance)) * vec3(0.3,0.2,0.0);
+    //grass/trees: green, at height -0.25, spread: -0.30 to -0.20
+    variance = 0.05;
+    color_height = -0.25;
+    height_material += exp((-pow((height-color_height),2))/(2*variance*variance)) * texture(tex3,uv).rgb;
+    //mountain: grey, at height 0.0, spread: -0.20 to 0.20
+    variance = 0.2;
+    color_height = 0.0;
+    height_material += exp((-pow((height-color_height),2))/(2*variance*variance)) * texture(tex2,uv).rgb;
+    //mountain top: white, at height 0.25, spread: 0.20 to 0.25
+    variance = 0.05;
+    color_height = 0.25;
+    height_material += exp((-pow((height-color_height),2))/(2*variance*variance)) * vec3(1.0,1.0,1.0);
+    if(height < -0.45) {
+        height_material = vec3(0.0,0.0,0.7); // water
+    }
+    if(height > 0.25) {
+        height_material = vec3(1.0,1.0,1.0); // snow
+    }
+
+    material = height_material;
 
 }
